c3fdfb0b31d836663da3203265bede85735a9a29
Sajid <mdkumar591@gmail.com>
2023-03-29 08:35:26 +0530

    Bug fix

--- app.py
+++ app.py
@@ -1,10 +1,5 @@
 from flask import Flask, render_template, request
-import jsonify
-import requests
 import pickle
-import numpy as np
-import sklearn
-from sklearn.preprocessing import StandardScaler
 
 app = Flask(__name__)
 model = pickle.load(open('test.pkl', 'rb'))
